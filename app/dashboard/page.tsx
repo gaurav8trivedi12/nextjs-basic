121a2847a7932849d582e54905b85e771f3b5398
Gaurav Trivedi <gaurav8trivedi12@gmail.com>
2024-07-31 23:02:33 +0530

    dashboard page updated

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,8 +1,16 @@
 
 import ContactUsForm from '@/app/dashboard/Contact.client';
 import Navbar from '@/app/dashboard/Navbar';
-import { Archive } from 'lucide-react';
+import { Archive, CircleCheck, ThumbsUp } from 'lucide-react';
 import React from 'react';
+
+const GetReviewsButton = () => {
+    return (
+        <button className="text-lg mt-6 py-4 px-6 bg-blue-600 hover:bg-blue-700 rounded-full text-white font-bold shadow-lg tracking-wider">
+            Get Reviews for my SaaS
+        </button>
+    )
+}
 
 const LandingPage = () => {
     return (
@@ -17,9 +25,7 @@ const LandingPage = () => {
                     <p className="my-8 text-xl md:text-2xl text-gray-600 max-w-3xl">
                         Our automated review generation system helps you climb to the top of G2, Capterra, and Gartner in just 3 months.
                     </p>
-                    <button className="text-lg mt-6 py-4 px-6 bg-blue-600 hover:bg-blue-700 rounded-full text-white font-bold shadow-lg tracking-wider">
-                        Get Reviews for my SaaS
-                    </button>
+                    <GetReviewsButton />
                 </div>
 
                 <div className='text-center'>
@@ -27,7 +33,7 @@ const LandingPage = () => {
                 </div>
 
                 {/* Challenges Section */}
-                <div id="challenges" className="py-12 px-6 min-h-screen flex flex-col justify-center">
+                {/* <div id="challenges" className="py-12 px-6 min-h-screen flex flex-col justify-center">
                     <div className="max-w-4xl mx-auto">
                         <h2 className="text-3xl font-semibold text-gray-800">Struggling to Get Reviews for Your B2B SaaS?</h2>
                         <div className="mt-6 grid grid-cols-1 md:grid-cols-2 gap-4">
@@ -40,7 +46,6 @@ const LandingPage = () => {
                                             <div className="w-16 h-16 bg-gray-300 rounded-md">
                                                 <Archive />
                                             </div>
-                                            {/* Image placeholder */}
                                         </div>
                                         <div className="flex-1 min-w-0">
                                             <p className="text-gray-600 text-sm">{item}</p>
@@ -49,10 +54,11 @@ const LandingPage = () => {
                                 ))}
                         </div>
                     </div>
-                </div>
+                </div> */}
+                <StrugglingSection />
 
                 {/* Automated System Section */}
-                <div id="how-it-works" className="py-12 bg-gray-50 px-6 min-h-screen flex flex-col justify-center">
+                {/* <div id="how-it-works" className="py-12 bg-gray-50 px-6 min-h-screen flex flex-col justify-center">
                     <div className="max-w-4xl mx-auto">
                         <h2 className="text-3xl font-semibold text-gray-800">Our Automated Review Generation System Makes It Easy</h2>
                         <div className="mt-6 grid grid-cols-1 md:grid-cols-2 gap-4">
@@ -63,7 +69,7 @@ const LandingPage = () => {
                                 .map(item => (
                                     <div key={item} className="bg-white p-4 shadow-lg rounded-lg flex items-center space-x-4">
                                         <div className="flex-shrink-0">
-                                            <div className="w-16 h-16 bg-gray-300 rounded-md"></div> {/* Image placeholder */}
+                                            <div className="w-16 h-16 bg-gray-300 rounded-md"></div> 
                                         </div>
                                         <div className="flex-1 min-w-0">
                                             <p className="text-gray-600 text-sm">{item}</p>
@@ -72,7 +78,8 @@ const LandingPage = () => {
                                 ))}
                         </div>
                     </div>
-                </div>
+                </div> */}
+                <AutomatedSystemSection />
 
                 {/* Results Section */}
                 <div id="results" className="py-12 px-6 min-h-screen flex flex-col justify-center">
@@ -84,7 +91,7 @@ const LandingPage = () => {
                                 .map(item => (
                                     <div key={item} className="bg-white p-4 shadow-lg rounded-lg flex items-center space-x-4">
                                         <div className="flex-shrink-0">
-                                            <div className="w-16 h-16 bg-gray-300 rounded-md"></div> {/* Image placeholder */}
+                                            <div className="w-16 h-16 bg-gray-300 rounded-md"></div>
                                         </div>
                                         <div className="flex-1 min-w-0">
                                             <p className="text-gray-600 text-sm">{item}</p>
@@ -94,6 +101,8 @@ const LandingPage = () => {
                         </div>
                     </div>
                 </div>
+
+                <HowItWorks />
 
                 <ContactUsForm />
 
@@ -105,5 +114,110 @@ const LandingPage = () => {
         </>
     );
 }
+
+const StrugglingSection = () => {
+    const issues = [
+        "You know reviews are crucial for building trust and attracting new customers, but getting them consistently is a challenge.",
+        "You don't have the time or resources to manually reach out to customers and request reviews.",
+        "You're not sure how to encourage customers to leave reviews on the platforms that matter most, like G2, Capterra, and Gartner."
+    ];
+
+    return (
+        <div className="py-24 px-6">
+            <div className="px-24 mx-auto">
+                <h2 className="text-5xl font-bold text-gray-800 mb-6 text-center mb-12">Struggling to Get Reviews<br /> for Your B2B SaaS?</h2>
+                <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+                    {issues.map((issue, index) => (
+                        <div key={index} className="bg-blue-100 p-12 rounded-lg flex flex-col space-y-4">
+                            <CircleCheck className="text-blue-700 w-12 h-12" /> {/* Icon with styling */}
+                            <p className="text-gray-600">{issue}</p>
+                        </div>
+                    ))}
+                </div>
+            </div>
+        </div>
+    );
+};
+
+const AutomatedSystemSection = () => {
+    const features = [
+        "We integrate seamlessly with your existing customer data and identify your happiest customers.",
+        "Our system automatically sends personalized review requests to these satisfied customers on a regular basis.",
+        "We continuously monitor and optimize the messaging to ensure maximum review generation.",
+        "You sit back and watch as the reviews come in and your rankings improve on top review sites."
+    ];
+
+    return (
+        <div className="py-12 px-6">
+            <div className="max-w-4xl mx-auto">
+                <h2 className="text-5xl font-bold text-gray-800 text-center mb-12">Our Automated Review Generation<br /> System Makes It Easy</h2>
+                <div className="space-y-6">
+                    {features.map((feature, index) => (
+                        <div key={index} className={`bg-white p-6 rounded-lg shadow-md transform transition duration-500 ease-in-out`}
+                        >
+                            <div className="flex items-center space-x-4">
+                                <div className="p-2 rounded-full bg-blue-100"> {/* Light shade of theme color */}
+                                    <ThumbsUp className="w-6 h-6 text-blue-500" /> {/* Icon with theme color */}
+                                </div>
+                                <p className="text-gray-600 flex-1">{feature}</p>
+                            </div>
+                        </div>
+                    ))}
+                </div>
+            </div>
+        </div>
+    );
+};
+
+const HowItWorks = () => {
+    const steps = [
+        {
+            number: '01',
+            heading: "Step One: Integration",
+            text: "Our system integrates seamlessly with your existing platforms without any hassle."
+        },
+        {
+            number: '02',
+            heading: "Step Two: Identification",
+            text: "Automatically identify your happiest customers based on their interaction and feedback."
+        },
+        {
+            number: '03',
+            heading: "Step Three: Automation",
+            text: "Send out personalized review requests automatically to the identified satisfied customers."
+        }
+    ];
+
+    return (
+        <section className='bg-blue-100 py-24'>
+            <div className="container mx-auto my-24 px-4">
+                <div className="flex flex-col md:flex-row items-start justify-between space-y-8 md:space-y-0">
+                    <div className="md:w-1/2 flex flex-col justify-between">
+                        <div>
+                            <h2 className="text-4xl font-bold text-gray-800">How It Works?</h2>
+                            <p className="text-lg  my-4">Discover our simple and efficient process.</p>
+                        </div>
+                        <div>
+                            <GetReviewsButton />
+                        </div>
+                    </div>
+                    <div className="md:w-1/2 space-y-12">
+                        {steps.map(step => (
+                            <div key={step.number} className="flex items-start space-x-4">
+                                <div className="text-2xl font-bold text-white bg-blue-500 rounded-full h-14 w-16 flex items-center justify-center">
+                                    {step.number}
+                                </div>
+                                <div>
+                                    <h3 className="text-xl font-semibold ">{step.heading}</h3>
+                                    <p className="xxx-600">{step.text}</p>
+                                </div>
+                            </div>
+                        ))}
+                    </div>
+                </div>
+            </div>
+        </section >
+    );
+};
 
 export default LandingPage;
